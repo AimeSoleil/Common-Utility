e340b1190ae7b17406e40bcd9fea1244b0fd3e44
julian <juliansxw@users.noreply.github.com>
2018-04-27 16:00:28 +0800

    Simple implementation for retry logic with lambda
    Simple implementation for retry logic with java8 lambda

---
+++ RetryCommand.java
@@ -0,0 +1,63 @@
+package com.expedia.lis.luisbot.processor;
+
+import java.util.function.Supplier;
+
+public class RetryCommand<T> {
+    public static final int DEFAULT_MAX_RETRY_COUNT = 5;
+    public static final int DEFAULT_TIME_TO_WAIT = 1000;
+
+    private int retryCounter;
+    private int maxRetries;
+    private long timeToWait;
+    private Supplier<T> task;
+
+    public RetryCommand(Supplier<T> task) {
+        this.maxRetries = DEFAULT_MAX_RETRY_COUNT;
+        this.timeToWait = DEFAULT_TIME_TO_WAIT;
+        this.task = task;
+    }
+
+    public RetryCommand(int maxRetries, long timeToWait, Supplier<T> task) {
+        this.maxRetries = maxRetries;
+        this.timeToWait = timeToWait;
+        this.task = task;
+    }
+
+    public T runOnException(Class<? extends Throwable> targetEx) throws InterruptedException {
+        try {
+            return task.get();
+        } catch (Throwable e) {
+            if (targetEx.isInstance(e)) {
+                return retry(targetEx);
+            }
+
+            throw  e;
+        }
+    }
+
+    public int getRetryCounter() {
+        return this.retryCounter;
+    }
+
+    private T retry(Class targetEx) throws InterruptedException {
+        retryCounter = 0;
+        while (retryCounter <= maxRetries) {
+            try {
+                return task.get();
+            } catch (Throwable e) {
+                retryCounter++;
+                if (targetEx.isInstance(e)) {
+                    if (getRetryCounter() > maxRetries) {
+                        throw e;
+                    }
+
+                    Thread.sleep(timeToWait * getRetryCounter());
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        return null;
+    }
+}
